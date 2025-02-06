8e4cbcb445a2a93834426643e82441a14f886cfe
Shoaib Tahir <154751161+ShoaibTahir1@users.noreply.github.com>
2025-02-06 21:43:58 +0500

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import popularcar from '../popularcar';
+import recommendedcar from '../recommendedcar';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [popularcar, recommendedcar],
 }
